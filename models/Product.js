998ced0a26b847b3a772c70cc582c4bc32bfa274
unknown <coolshiv0721@gmail.com>
2022-06-20 18:27:23 +0530

    Created Product Model

---
+++ models/Product.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  brand: {
+    type: String,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now(),
+  },
+  updated: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = Product;
